4ed99b465d968f904c362336d7b26cafe56c820c
anthony <anthonygarcia2021@gmail.com>
2024-01-18 19:43:44 -0600

    fixed code.

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react";
 
 function SelectedContact({ selectedContactId, setSelectedContactId }) {
@@ -9,8 +10,8 @@ function SelectedContact({ selectedContactId, setSelectedContactId }) {
           `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`
         );
         const result = await response.json();
-        setContact(result);
-        // console.log(result);
+        setContact(() => result);
+        console.log(result);
       } catch (error) {
         console.error(error);
       }
@@ -19,9 +20,15 @@ function SelectedContact({ selectedContactId, setSelectedContactId }) {
   }, []);
   return (
     <>
-      <h1>Selected Contact</h1>
-      {console.log(contact)}
-      <p>{contact.name}</p>
+      <div>
+        <h1>Selected Contact</h1>
+        <p>{contact?.name}</p>
+        <p>{contact?.username}</p>
+        <p>{contact?.phone}</p>
+        <p>{contact?.website}</p>
+        <p>{contact?.address.city}</p>
+        {console.log(contact)}
+      </div>
     </>
   );
 }
